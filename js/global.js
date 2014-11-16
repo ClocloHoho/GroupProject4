aa5b68bfde412cdf1784182ddadf0cb5d894222b
ClocloHoho <clotilde.hovine@gmail.com>
2014-11-16 20:08:58 +0000

    Add an action to see the login page

--- js/global.js
+++ js/global.js
@@ -5,13 +5,18 @@ var RegisterFormUserPasswordOk = false;
 
 function showRegisterPopup(){
     document.getElementById("registerPopup").style.visibility = 'visible';
-    document.getElementById("actualPage").style.opacity = "0.4";
+    /*document.getElementById("actualPage").style.opacity = "0.4";*/
     document.getElementById("registerPopup").style.zIndex = "10";
 }
 function hideRegisterPopup(){
     document.getElementById("registerPopup").style.visibility = 'hidden';
     document.getElementById("actualPage").style.opacity = "1";
     document.getElementById("registerPopup").style.zIndex = "0";
+}
+function showLogin(){
+    document.getElementById("login").style.visibility = 'visible';
+    /*document.getElementById("actualPage").style.opacity = "0.4";*/
+    document.getElementById("login").style.zIndex = "10";
 }
 function registerFormCheckUsername(){
     document.getElementById("registerFormUsername").className = "registerFormField registerFormFieldLoading";
