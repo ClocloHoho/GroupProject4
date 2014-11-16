e20af3c84b38c785858e0a53e07a313fcf9a507c
ClocloHoho <clotilde.hovine@gmail.com>
2014-11-16 18:55:04 +0000

    Register skeleton
    Add a new presentation for the register file.

--- registerPopup.php
+++ registerPopup.php
@@ -1,31 +1,29 @@
-<div id="registerPopup">
-    <form action="tools/register.php" method="post" class="registerForm">
-        <div class="registerFormLine">
-            <label for="registerFormUsername" class="registerFormLabel">Username</label>
-            <input type="text" name="userName" placeholder="Username" id="registerFormUsername" class="registerFormField" onchange="registerFormCheckUsername()">
-        </div>
-        <div class="registerFormLine">
-            <label for="registerFormPassword" class="registerFormLabel">Password</label>
-            <input type="password" name="password" placeholder="Password" id="registerFormPassword" class="registerFormField" onchange="registerFormCheckConfirmPassword()">
-        </div>
-        <div class="registerFormLine">
-            <label for="registerFormPasswordConfirm" class="registerFormLabel">Confirm password</label>
-            <input type="password" name="passwordConfirm" placeholder="Confirm password" id="registerFormPasswordConfirm" class="registerFormField" onchange="registerFormCheckConfirmPassword()">
-        </div>
-        <div class="registerFormLine">
-            <label for="registerFormEmail" class="registerFormLabel">E-mail</label>
-            <input type="email" name="email" placeholder="E-mail" id="registerFormEmail" class="registerFormField" onchange="registerFormCheckConfirmEmail()">
-        </div>
-        <div class="registerFormLine">
-            <label for="registerFormEmailConfirm" class="registerFormLabel">Confirm e-mail</label>
-            <input type="email" name="emailConfirm" placeholder="Confirm e-mail" id="registerFormEmailConfirm" class="registerFormField" onchange="registerFormCheckConfirmEmail()">
-        </div>
-        <div class="registerFormLine">
-            <div class="registerFormSubmitButtonDiv">
-                <input type="submit" value="Register" class="registerFormSubmitButton" id="registerFormSubmitButton" disabled/></div
-            ><div class="registerFormSubmitButtonDiv">
-                <button type="button" class="registerFormSubmitButton" onclick="hideRegisterPopup()">Cancel</button>
-            </div>
-        </div>
-    </form>
-</div>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Eat in Cork</title>
+        <link rel="stylesheet" href="../css/login.css">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+    </head>
+
+    <body>
+   	<div id="container" class="pt50 pb50"> 
+   	<h1>Register</h1>
+   	<h2 class="title-xs">Join us for more utilities</h2>
+   	
+   	<form action="tools/register.php" method="post">
+   		<p class="title-form title-xs">Name</p>
+   		<input type="text" name="userName" class="input w100 mt10" placeholder="Name" onchange="registerFormCheckUsername()" maxlenght="5110">
+   		<p class="title-form title-xs">Email</p>
+   		<input type="email" name="email" class="input w100" placeholder="Email" maxlenght="256" onchange="registerFormCheckConfirmEmail()">
+   		<input type="email" name="emailConfirm" class="input w100 mt10" placeholder="Confirm Email" maxlenght="256" onchange="registerFormCheckConfirmEmail()">
+   		<p class="title-form title-xs">Password</p>
+   		<input type="password" name="password" class="input w100 mt10" placeholder="Password" onchange="registerFormCheckConfirmPassword()">
+   		<input type="password" name="passwordConfirm" class="input w100 mt10" placeholder="Confirm Password" onchange="registerFormCheckConfirmPassword()">
+   		<input type="submit" class="button w100" value="Continue">
+   	</form>
+   	<button type="button" class="button w100 mt15" onclick="hideRegisterPopup()">Cancel</button>
+	</div> 
+    </body>
+</html>
