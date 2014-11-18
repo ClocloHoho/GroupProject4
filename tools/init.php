201a41739918385a314d9f4ecf6d225362587d35
ClocloHoho <clotilde.hovine@gmail.com>
2014-11-18 20:33:53 +0000

    Problem with the password
    Is it possible to make an exception for the password ?

--- tools/init.php
+++ tools/init.php
@@ -16,7 +16,7 @@ if(isset($_SESSION['id_user'])){
 
 //handles database connection
 //TODO : better exceptions
-$dbhandle = mysql_connect("127.0.0.1", "root", "root")
+$dbhandle = mysql_connect("127.0.0.1", "root", "")
   or die("Unable to connect to MySQL");
 $groupProjectDB = mysql_select_db("groupproject",$dbhandle)
   or die("Could not select db");
