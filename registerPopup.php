3dc5edfa5f3fb228d0e179928c8bd21a583cd7b1
ClocloHoho <clotilde.hovine@gmail.com>
2014-11-19 18:09:22 +0000

    Improve the CSS
    Change the color of the image.
    Add the image in register.
    Add some media queries

--- registerPopup.php
+++ registerPopup.php
@@ -12,20 +12,34 @@
    	<h1>Register</h1>
    	<h2 class="title-xs">Join us for more utilities</h2>
    	
-   	<form action="tools/register.php" method="post">
-   		<label for="registerFormUsername" class="registerFormLabel title-form title-xs">Name</label>
-   		<input type="text" name="userName" class="registerFormField input w100 mt10" placeholder="Name" onchange="registerFormCheckUsername()" maxlenght="5110">
-   		<label for="registerFormEmail" class="title-form title-xs registerFormLabel">Email</label>
-   		<input type="email" name="email" class="registerFormField input w100" placeholder="Email" maxlenght="256" onchange="registerFormCheckConfirmEmail()">
-   		<input type="email" name="emailConfirm" class="registerFormEmailConfirm input w100 mt10" placeholder="Confirm Email" maxlenght="256" onchange="registerFormCheckConfirmEmail()">
-   		<label for="registerFormPassword" class="registerFormLabel title-form title-xs">Password</label>
-   		<input type="password" name="password" class="registerFormField input w100 mt10" placeholder="Password" onchange="registerFormCheckConfirmPassword()">
-   		<input type="password" name="passwordConfirm" class="registerFormPasswordConfirm input w100 mt10" placeholder="Confirm Password" onchange="registerFormCheckConfirmPassword()">
-   		<input type="submit" class="button w100" value="Continue">
+   	<form action="tools/register.php" method="post" class="registerForm">
+   		<div class="registerFormLine">
+   			<label for="registerFormUsername" class="registerFormLabel title-form title-xs">Name</label>
+   			<input type="text" name="userName" class="registerFormField input w100 mt10" placeholder="Name" onchange="registerFormCheckUsername()" maxlenght="5110">
+   		</div>
+   		<div class="registerFormLine">
+   			<label for="registerFormEmail" class="title-form title-xs registerFormLabel">Email</label>
+   			<input type="email" name="email" id="registerFormEmail" class="registerFormField input w100" placeholder="Email" maxlenght="256" onchange="registerFormCheckConfirmEmail()">
+   		</div>
+   		<div>
+   			<input type="email" name="emailConfirm" id="registerFormEmailConfirm" class="registerFormEmailConfirm input w100 mt10" placeholder="Confirm Email" maxlenght="256" onchange="registerFormCheckConfirmEmail()">
+   		</div>
+   		<div class="registerFormLine">
+   			<label for="registerFormPassword" class="registerFormLabel title-form title-xs">Password</label>
+   			<input type="password" name="password" id="registerFormPassword" class="registerFormField input w100 mt10" placeholder="Password" onchange="registerFormCheckConfirmPassword()">
+   		</div>
+   		<div class="registerFormLine">
+   			<input type="password" name="passwordConfirm" id="registerFormPasswordConfirm" class="registerFormPasswordConfirm input w100 mt10" placeholder="Confirm Password" onchange="registerFormCheckConfirmPassword()">
+   		</div>
+   		<div class="registerFormLine">
+            <div class="registerFormSubmitButtonDiv">
+                <input type="submit" value="Continue" class="registerFormSubmitButton button w100" id="registerFormSubmitButton" disabled/>
+            </div>
+            <div class="registerFormSubmitButtonDiv">
+                <button type="button" class="registerFormSubmitButton button w100 mt15" onclick="hideRegisterPopup()">Cancel</button>
+            </div>
+        </div>
    	</form>
-    <a href="index.php" style="border-bottom:0px;">
-   	    <button type="button" class="button w100 mt15">Cancel</button>
-    </a>
 	</div> 
     </body>
 </html>
